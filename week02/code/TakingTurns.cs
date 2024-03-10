36f154972b710f5344d6297ddfa26cfa902ebec6
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-03-09 21:08:37 -0500

    finished first half of assignment

--- week02/code/TakingTurns.cs
+++ week02/code/TakingTurns.cs
@@ -1,5 +1,7 @@
-﻿public static class TakingTurns {
-    public static void Test() {
+﻿public static class TakingTurns
+{
+    public static void Test()
+    {
         // TODO Problem 1 - Run test cases and fix the code to match requirements
         // Test Cases
 
@@ -12,11 +14,11 @@
         players.AddPerson("Bob", 2);
         players.AddPerson("Tim", 5);
         players.AddPerson("Sue", 3);
-        // Console.WriteLine(players);    // This can be un-commented out for debug help
+        Console.WriteLine(players);    // This can be un-commented out for debug help
         while (players.Length > 0)
             players.GetNextPerson();
         // Defect(s) Found: 
-
+        //the queue adds the names repeatedly instead of dequeueing and adding it back in. Cahnged .instert to .add(person) to fix the order of the queue being printed
         Console.WriteLine("---------");
 
         // Test 2
@@ -28,7 +30,9 @@
         players.AddPerson("Bob", 2);
         players.AddPerson("Tim", 5);
         players.AddPerson("Sue", 3);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
+        {
+
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
@@ -39,7 +43,7 @@
             players.GetNextPerson();
 
         // Defect(s) Found: 
-
+        //no defects found 
         Console.WriteLine("---------");
 
         // Test 3
@@ -52,15 +56,16 @@
         players.AddPerson("Tim", 0);
         players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
         // Defect(s) Found: 
-
+        // result says there isnt anyone in queue after first time. added a if statement to GetNextPerson to handle infinite turns
         Console.WriteLine("---------");
 
-         // Test 4
+        // Test 4
         // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
         // Run 10 times.
         // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
@@ -69,12 +74,13 @@
         players.AddPerson("Tim", -3);
         players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
         // Defect(s) Found: 
-
+        // no defects found
         Console.WriteLine("---------");
 
         // Test 5
@@ -83,6 +89,6 @@
         Console.WriteLine("Test 5");
         players = new TakingTurnsQueue();
         players.GetNextPerson();
-        // Defect(s) Found:
+        // Defect(s) Found: no defects found. error message is correct.
     }
 }
