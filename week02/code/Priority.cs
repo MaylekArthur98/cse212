4add5bdf6fc790d6790aa7e30dc32dcd9b951606
MaylekArthur98 <Maylek.arthur@gmail.com>
2024-05-07 20:40:35 -0400

    worked on fixing the code

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -1,5 +1,7 @@
-﻿public static class Priority {
-    public static void Test() {
+﻿public static class Priority
+{
+    public static void Test()
+    {
         // TODO Problem 2 - Write and run test cases and fix the code to match requirements
         // Example of creating and using the priority queue
         var priorityQueue = new PriorityQueue();
@@ -11,7 +13,11 @@
         // Scenario: 
         // Expected Result: 
         Console.WriteLine("Test 1");
-
+        priorityQueue.Enqueue("bob,", 10);
+        priorityQueue.Enqueue("jim", 9);
+        priorityQueue.Enqueue("sally", 6);
+        priorityQueue.Enqueue("Noel", 14);
+        Console.WriteLine(priorityQueue);
         // Defect(s) Found: 
 
         Console.WriteLine("---------");
@@ -20,7 +26,8 @@
         // Scenario: 
         // Expected Result: 
         Console.WriteLine("Test 2");
-
+        priorityQueue.Dequeue();
+        Console.WriteLine(priorityQueue);
         // Defect(s) Found: 
 
         Console.WriteLine("---------");
